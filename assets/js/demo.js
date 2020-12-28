bfef985b589632c06e61a3bebdcdc4cb521389e9
jithinb68 <jithinb68@Jithin.local>
2020-12-28 20:20:27 +0530

    datepicker in modal

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -8,3 +8,18 @@ $('.owl-carousel').owlCarousel({
     navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
     dots:false
 });
+
+if ($("#datepicker-arrival").length) {
+    $('#datepicker-arrival').datepicker({
+      enableOnReadonly: true,
+      todayHighlight: true,
+    });
+}
+
+
+if ($("#datepicker-departure").length) {
+    $('#datepicker-departure').datepicker({
+      enableOnReadonly: true,
+      todayHighlight: true,
+    });
+}

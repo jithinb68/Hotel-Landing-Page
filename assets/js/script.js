ae9c98dee31189daf63de8cc37d5a0a7273317f4
jithinb68 <jithinb68@Jithin.local>
2020-12-31 09:49:12 +0530

    width, room carousel, navbar width

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,73 @@ if ($('.owl-carousel').length) {
     }
   });
 }
+
+
+if ($('.room-suite').length) {
+  $('.room-suite').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 4500,
+    navText: ["<i class='mdi mdi-chevron-left'></i>", "<i class='mdi mdi-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+}
+
+if ($('.room-executive').length) {
+  $('.room-executive').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 4500,
+    navText: ["<i class='mdi mdi-chevron-left'></i>", "<i class='mdi mdi-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+}
+
+if ($('.room-deluxe').length) {
+  $('.room-deluxe').owlCarousel({
+    loop: true,
+    margin: 10,
+    dots: true,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 4500,
+    navText: ["<i class='mdi mdi-chevron-left'></i>", "<i class='mdi mdi-chevron-right'></i>"],
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
+    }
+  });
+}
